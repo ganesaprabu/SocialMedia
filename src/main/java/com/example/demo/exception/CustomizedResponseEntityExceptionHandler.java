e936d00f372e3f832e5b0b6c1d5a25d289eecced
ganesaprabu <ganesa.tech@gmail.com>
2019-06-27 07:04:45 +0530

    Changed the package

--- src/main/java/com/example/demo/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/demo/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.example.demo.user.exception;
+package com.example.demo.exception;
 
 import java.util.Date;
 
--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.user.exception.UserNotFoundException;
+import com.example.demo.exception.UserNotFoundException;
 
 @RestController
 public class UserController {
